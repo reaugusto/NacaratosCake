a972f5cfc7f4dea305875b0d4372c7fbb1048598
Renato Augusto <renato.dexter@gmail.com>
2018-12-07 20:25:28 -0200

    front login/cadastro

--- Js/cadastrar.js
+++ Js/cadastrar.js
@@ -1,3 +1,19 @@
+$( "input" ).focusin(function() {
+  $( this ).find( "span" ).animate({"opacity":"0.0"}, 200);
+});
+
+$( "input" ).focusout(function() {
+  $( this ).find( "span" ).animate({"opacity":"1"}, 300);
+});
+
+$(".login").submit(function(){
+  $(this).find(".submit i").removeAttr('class').addClass("fas fa-check").css({"color":"#fff"});
+  $(".submit").css({"background":"#2ecc71", "border-color":"#2ecc71"});
+  $(".feedback").show().animate({"opacity":"1", "bottom":"-80px"}, 400);
+  $("input").css({"border-color":"#2ecc71"});
+  return false;
+});
+
 var config = {
     apiKey: "AIzaSyCVFt-wU_Ap8hNj3YUrJCq7CFXA7TYiOc0",
     authDomain: "nacarato-s.firebaseapp.com",
@@ -21,4 +37,5 @@ function cadastrar(){
         var errorMessage = error.message;
         // ...
     });
+    
 }
